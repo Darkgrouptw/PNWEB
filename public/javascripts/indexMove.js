e2c36a3d10e212786ad900dff8e8c6305cba0c16
Graphics <darkgrouptw@gmail.com>
2015-11-19 20:38:25 +0800

    新增登入頁面 部份頁面繁體化

--- public/javascripts/indexMove.js
+++ public/javascripts/indexMove.js
@@ -12,34 +12,36 @@ $(function()
 			{
 				if(first)
 					$('.imagebox').append($('.imagebox img').first());
+
 				$('.imagebox img').last().css({'width' :  imageboxWidth + "px"});
-                $('.imagebox img').first().animate({'width':'0px'},1000);
+        $('.imagebox img').first().animate({'width':'0px'},1000);
 				first = true;
 
-                for(var i = 0 ; i < $(".iconbox span").size() ; i++)
-                	$(".iconbox span").eq(i).removeClass("active");
+         for(var i = 0 ; i < $(".iconbox span").size() ; i++)
+         	$(".iconbox span").eq(i).removeClass("active");
 
-                index = (index +1) % 4;
-                $('.iconbox span').eq(index).attr("class","active");
+        index = (index +1) % 4;
+        $('.iconbox span').eq(index).attr("class","active");
 			},3000);
 
 
-	$(".iconbox span").click(function(){
+	$(".iconbox span").click(function()
+  {
 		first = false;
 		index = parseInt($(this).attr("rel"));
 
-        for(var i = 0 ; i < $(".iconbox span").size() ; i++)
-        {
-          	$(".iconbox span").eq(i).removeClass("active");
-			$('.imagebox img').eq(i).css({'width' :  imageboxWidth + "px"});
-        }
-        $('.iconbox span').eq(index).attr("class","active");
+    for(var i = 0 ; i < $(".iconbox span").size() ; i++)
+    {
+      $(".iconbox span").eq(i).removeClass("active");
+	    $('.imagebox img').eq(i).css({'width' :  imageboxWidth + "px"});
+    }
+    $('.iconbox span').eq(index).attr("class","active");
 
-        for(var i = 0 ; i < $(".imagebox img").size() ; i++)
-        {
-        	if((index + 1) == $(".imagebox img").first().attr("alt"))
-        		break;
-			$('.imagebox').append($('.imagebox img').first());
-        }
+    for(var i = 0 ; i < $(".imagebox img").size() ; i++)
+    {
+     	if((index + 1) == $(".imagebox img").first().attr("alt"))
+     		break;
+	   	$('.imagebox').append($('.imagebox img').first());
+    }
 	});
 });
