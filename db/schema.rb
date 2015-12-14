18a9841bea76636b10a6bb4b74a813da91f2b3b8
jackywu <apple970066@gmail.com>
2015-12-14 12:53:50 +0800

    新增job頭銜

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119120325) do
+ActiveRecord::Schema.define(version: 20151214044823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20151119120325) do
     t.text     "content"
     t.integer  "count"
     t.string   "link"
-	t.string   "job"
+    t.string   "job"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
