ee2d93c61481ce79b8b7f6ef2b15b38c688d73fe
Graphics <Graphics@GraphicsdeMac-mini.local>
2016-01-21 18:12:22 +0800

    Kasim

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,2 +1,9 @@
 class MainController < ApplicationController
+	def index
+		@contents = DataContent.all
+		@issues_OrderByUpdated=DataIssue.where(parent_id: -1).order(:updated_at)
+		@issues=DataIssue.all
+		@persons=DataPerson.all
+		@pointers=DataPointer.all
+	end
 end
