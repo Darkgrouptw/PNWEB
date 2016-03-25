e6ea580a0890d834898aff4b1a9139dd808b011b
Dark <darkgrouptw@gmail.com>
2016-03-25 22:29:19 +0800

    修正登入頁面，跟最高權限的問題

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,5 +4,16 @@ class MainController < ApplicationController
 		@issues=DataIssue.all
 		@persons=DataPerson.all
 		@pointers=DataPointer.all
+        
+        if user_signed_in?
+            logger.debug "!!!!! Has user login !!!!!"
+            if current_user.email == "darkgrouptw@gmail.com" || current_user.email == "b10215014@mail.ntust.edu.tw"
+                logger.debug "Is Admin"
+                #logger.debug 
+                User.where(email: current_user.email)[0].update(high_power: true)
+            else
+                User.where(email: current_user.email)[0].update(high_power: false)
+            end
+        end
 	end
 end
