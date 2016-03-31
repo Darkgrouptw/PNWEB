26ce16d1665831f4db073134c8c6bc528680e4d6
Graphics <Graphics@GraphicsdeMac-mini.local>
2016-03-31 22:19:22 +0800

    fix three 換位置的情況 新增 “我要議論按鈕”

--- app/controllers/issuelist_controller.rb
+++ app/controllers/issuelist_controller.rb
@@ -1,7 +1,7 @@
 class IssuelistController < ApplicationController
 	def index
 		@tags = params[:issue_id]
-		@issues=DataIssue.all
+		@issues=DataIssue.all.order(:created_at)
 		if @issues.where(id: @tags).length >= 1
 			@me = @issues.where(id: @tags)[0]
 			@datadetail=DataDetail.where(issue_id: @me.id)
