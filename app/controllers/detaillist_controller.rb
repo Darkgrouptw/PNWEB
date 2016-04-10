8113c6553ce9b09d2dbfc5d698c527e74a174c01
Graphics <Graphics@GraphicsdeMac-mini.local>
2016-04-10 15:32:00 +0800

    user can give comment now ! !! !!! !!!@!!

--- app/controllers/detaillist_controller.rb
+++ app/controllers/detaillist_controller.rb
@@ -10,9 +10,15 @@ class DetaillistController < ApplicationController
 			@person = DataPerson.where(id: @me.people_id)[0]
 			@user = User.where(id: @me.post_id)[0]
 			if !@me.comment_id.nil? && !@me.comment_id.empty?
-				@strings = @me.comment_id.split(",")
+				@strings = @me.comment_id.split(/,/)
+				@comments = DataComment.where(id: @strings).order(:created_at).reverse
+				@strings2 = Array.new
+				@comments.each do |comment|
+					@strings2.push(comment.post_id)
+				end
+				@talkers = User.where(id: @strings2)
 			end
-			@comments = DataComment.where(:id => @strings)
+			
 			@all_detail = false
 		else
 			@all_detail = true
@@ -24,5 +30,28 @@ class DetaillistController < ApplicationController
 	end
 
 	def create
+		@comment = DataComment.create(comment_params)
+		@comment.post_id = current_user.id
+		if @comment.save
+			@tags = params[:detail_id]
+			@detail = DataDetail.where(:id => @tags)[0]
+			@detail.comment_id = @detail.comment_id + @comment.id.to_s + ","
+			@detail.update(comment_id: @detail.comment_id)
+			redirect_to detaillist_path+"/"+@tags
+		else
+			render :new
+		end
+    end
+
+    def detail_params
+        params.require(:data_detail).permit(:is_support, :content, :link, :count, :count_like, :count_dislike, :post_id, :people_id, :issue_id, :comment_id, :comment_id, :issue_id)
+    end
+    
+    def issue_params
+        params.require(:data_detail).permit(:datadetail)
+    end
+
+    def comment_params
+    	params.require(:data_comment).permit(:content)
     end
 end
