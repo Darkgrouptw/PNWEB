34be66f169cb8418af2aee215ef3649985d3af4f
Dark <darkgrouptw@gmail.com>
2016-06-23 20:26:57 +0800

    1. 修正小錯誤

--- app/controllers/reportlist_controller.rb
+++ app/controllers/reportlist_controller.rb
@@ -3,7 +3,7 @@ class ReportlistController < ApplicationController
     
     def index
         if current_user.high_power
-            //123
+            
         else
             redirect "/", flash[:warning]="必須要擁有最高權限！！"
         end
