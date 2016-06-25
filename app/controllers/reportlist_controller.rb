cb8b525d7181e61a6496b64e1b96e5e7bcc66904
Graphics <Graphics@GraphicsdeMac-mini.local>
2016-06-25 15:08:24 +0800

    檢舉頁面更新

--- app/controllers/reportlist_controller.rb
+++ app/controllers/reportlist_controller.rb
@@ -3,7 +3,20 @@ class ReportlistController < ApplicationController
     
     def index
         if current_user.high_power
-            
+            @reportDetail = ReportDetail.order(:created_at)
+            detail_ids = []
+            @reportDetail.each do |item|
+            	detail_id.push(item.detail_id)
+            end
+            @detail = DataDetail.where(id: detail_ids)
+            issue_ids = []
+            user_ids = []
+            @detail.each do |item|
+            	issue_ids.push(item.issue_ids)
+            	user_ids.push(item.post_id)
+            end
+            @issue = DataIssue.where(id: issue_ids)
+            @user = User.where(id: user_ids)
         else
             redirect "/", flash[:warning]="必須要擁有最高權限！！"
         end
