cc5bd202f356124ef943aa53a53bf91e57c51052
Dark <darkgrouptw@gmail.com>
2016-07-13 21:47:03 +0800

    1. 用原來的方式

--- app/controllers/user/register_controller.rb
+++ app/controllers/user/register_controller.rb
@@ -243,18 +243,12 @@ class User::RegisterController < ApplicationController
     
     # 寄信
     def send_email(uuid, email)
-        begin
-            RestClient.post "https://api:key-ddabee7356c4bbecc5c1846d6994a2ec"\
-                    "@api.mailgun.net/v3/sandbox2bbf267493614a8b843884423bc7a480.mailgun.org/messages",
-                    :from => "正反網頁 <postmaster@sandbox2bbf267493614a8b843884423bc7a480.mailgun.org>",
-                    :to => "<" + email + ">",
-                    :subject => "正反網頁 - 信箱驗證信",
-                    :text => "親愛的 " + params[:email] + "你好：\n\t這是你的認證網址，請點擊下面網址，及繼續接下來的流程\nhttps://npweb.herokuapp.com/VerifyEmail?token="+uuid+"&email="+email
-        rescue
-            # 晚點傳
-            sleep(1)
-            send_email(uuid, email)
-        end 
+        RestClient.post "https://api:key-ddabee7356c4bbecc5c1846d6994a2ec"\
+                "@api.mailgun.net/v3/sandbox2bbf267493614a8b843884423bc7a480.mailgun.org/messages",
+                :from => "正反網頁 <postmaster@sandbox2bbf267493614a8b843884423bc7a480.mailgun.org>",
+                :to => "<" + email + ">",
+                :subject => "正反網頁 - 信箱驗證信",
+                :text => "親愛的 " + params[:email] + "你好：\n\t這是你的認證網址，請點擊下面網址，及繼續接下來的流程\nhttps://npweb.herokuapp.com/VerifyEmail?token="+uuid+"&email="+email
     end
     
     def verifylist_params
