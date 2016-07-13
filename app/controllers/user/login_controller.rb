c982093ffb40f32ef0c02e622e57cebf860622c3
Dark <darkgrouptw@gmail.com>
2016-07-13 20:26:12 +0800

    1. 讓登出按鈕有功能

--- app/controllers/user/login_controller.rb
+++ app/controllers/user/login_controller.rb
@@ -1,2 +1,15 @@
 class User::LoginController < ApplicationController
+    def login
+    end
+    
+    def logout
+        if session[:UserID] != nil
+            session.delete(:UserID)
+        end
+        if session[:UserToken] != nil
+            session.delete(:UserToken)
+        end
+        redirect_to "/"
+        flash[:notice] = "登出成功！！"
+    end
 end
