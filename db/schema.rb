0ac17918f3c93c54db06d204777342d0e9d7d43b
Dark <darkgrouptw@gmail.com>
2016-08-01 10:47:45 +0800

    記得 Rake db:reset

--- db/schema.rb
+++ db/schema.rb
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20160721082859) do
     t.integer  "trunk_id"
     t.integer  "popularity"
     t.string   "datadetail_id"
+    t.integer  "thumb_up"
     t.string   "tag"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
