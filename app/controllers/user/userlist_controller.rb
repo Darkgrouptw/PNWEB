9ba6a324c5a8a4d8d7a0cd9f7087a186fe8bc81c
Graphics <Graphics@GraphicsdeMac-mini.local>
2016-08-11 17:47:55 +0800

    更新使用者頁面

--- app/controllers/user/userlist_controller.rb
+++ app/controllers/user/userlist_controller.rb
@@ -1,4 +1,11 @@
 class User::UserlistController < ApplicationController
 	def index
+		@me = User.where(id: params[:id])[0]
+		@details = DataDetail.where(post_id: @me.id)
+		issue_ids = []
+		@details.each do |item|
+			issue_ids.push(item.issue_id)
+		end
+		@issues = DataIssue.where(id: issue_ids)
 	end
 end
