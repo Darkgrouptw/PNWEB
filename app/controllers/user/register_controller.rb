2467609c0d52f62293b28b662b3e03e7b113fb4b
Dark <darkgrouptw@gmail.com>
2016-08-12 15:46:34 +0800

    刪除多餘的 code

--- app/controllers/user/register_controller.rb
+++ app/controllers/user/register_controller.rb
@@ -246,23 +246,6 @@ class User::RegisterController < ApplicationController
     
     private
     
-    # 寄信
-    def send_email(uuid, email)
-        
-  #RestClient.post "https://api:YOUR_API_KEY"\
-  #"@api.mailgun.net/v3/YOUR_DOMAIN_NAME/messages",
-  #:from => "Excited User <mailgun@YOUR_DOMAIN_NAME>",
-  #:to => "bar@example.com, YOU@YOUR_DOMAIN_NAME",
-  #:subject => "Hello",
-  #:text => "Testing some Mailgun awesomness!"
-        #RestClient.post "https://api:key-ddabee7356c4bbecc5c1846d6994a2ec"\
-        #        "@api.mailgun.net/v3/sandbox2bbf267493614a8b843884423bc7a480.mailgun.org/messages",
-        #        :from => "正反網頁 <postmaster@sandbox2bbf267493614a8b843884423bc7a480.mailgun.org>",
-        #        :to => "<" + email + ">",
-        #        :subject => "正反網頁 - 信箱驗證信",
-        #        :text => "親愛的 " + params[:email] + "你好：\n\t這是你的認證網址，請點擊下面網址，及繼續接下來的流程\nhttps://npweb.herokuapp.com/VerifyEmail?token="+uuid+"&email="+email
-    end
-    
     def verifylist_params
         params.permit(:email)
     end
