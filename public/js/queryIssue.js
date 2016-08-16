73a48907a9abe2594f8ed658a5facdf5594e79f4
Dark <darkgrouptw@gmail.com>
2016-08-16 11:41:35 +0800

    !! 修正賢廣的 bug !! 1. 多了檢查 issue的 query 2. 產生 query.css 3. issuelist/add 是使用 找 issue 的 query

---
+++ public/js/queryIssue.js
@@ -0,0 +1,34 @@
+$(function(){
+    // 給 Issuelist 的 change 事件
+    $("#trunkID").on("focus input" ,function(){
+        $Name = $(this).prop("value");
+        if($Name != "")
+            $.get( "/IssueName", { name: $Name }).done( 
+                function( data ) {
+                    // 刪除前面的空白
+                    data = data.substr(4, data.length);
+                    // 刪除後面的換行
+                    data = data.substr(0, data.length - 1);
+                    data = data.split(",")
+                    $("#trunkIDDiv").empty();
+                    
+                    if(data.length == 1 && data[0] == "\n")
+                        return;
+                    
+                    for($i = 0; $i < data.length; $i++)
+                        $("#trunkIDDiv").append("<div onclick=clickQuery(this)>" + data[$i] + "</div>");
+                }
+            );
+    });
+    $("#trunkID").on("blur", function(){
+        setTimeout(function(){
+            $("#trunkIDDiv").empty();
+        },100);
+    });
+});
+
+function clickQuery(result)
+{
+    document.getElementById("trunkID").value = result.innerText;
+    result.parentNode.innerHTML = "";
+}
