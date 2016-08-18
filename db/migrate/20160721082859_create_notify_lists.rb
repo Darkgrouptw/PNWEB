91d5d1d82a126a9c00cd4e1f503cc35470daff78
Dark <darkgrouptw@gmail.com>
2016-08-18 13:53:52 +0800

    修正賢廣說的時間格式問題

--- db/migrate/20160721082859_create_notify_lists.rb
+++ db/migrate/20160721082859_create_notify_lists.rb
@@ -3,8 +3,8 @@ class CreateNotifyLists < ActiveRecord::Migration
     create_table :notify_lists do |t|
       t.integer :user_id
       t.integer :issue_id
-      t.time :last_read
-      t.time :newest_detail
+      t.datetime :last_read
+      t.datetime :newest_detail
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -74,8 +74,8 @@ ActiveRecord::Schema.define(version: 20160721082859) do
   create_table "notify_lists", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "issue_id"
-    t.time     "last_read"
-    t.time     "newest_detail"
+    t.datetime "last_read"
+    t.datetime "newest_detail"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
