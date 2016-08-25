0154f8d8ed47221c07a140b081f6accb04f43b11
Graphics <Graphics@GraphicsdeMac-mini.local>
2016-08-25 14:11:12 +0800

    Facebook share function

--- public/js/queryToggle.js
+++ public/js/queryToggle.js
@@ -72,4 +72,18 @@ function NegToggle(){
 	$("#negContent").toggle();
 }
 
+ window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1175473039193082',
+      xfbml      : true,
+      version    : 'v2.7'
+    });
+  };
 
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
