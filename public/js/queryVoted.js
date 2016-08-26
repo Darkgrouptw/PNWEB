9906fddcfa85246e10630bfb92e07d627b3f64df
Graphics <Graphics@GraphicsdeMac-mini.local>
2016-08-26 21:59:29 +0800

    新增讓使用者觀看投過的論述

---
+++ public/js/queryVoted.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    // 要將 ReportModal 裡面的 Tile 加上，編輯者，主要論述
+    $(".ReportModal_Voted").each(function(){
+        $(this).click(function(){
+        $("#ReportForm").attr("action", "/Reportlist/new" );
+        $("#detail_id").attr("value", detail_id);
+        })
+    })
+})
