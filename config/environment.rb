8d57b4a8ffd02ad23439660a82002744b762eab7
Dark <darkgrouptw@gmail.com>
2016-10-31 17:16:37 +0800

    DB 修正

--- config/environment.rb
+++ config/environment.rb
@@ -11,5 +11,6 @@ ActionMailer::Base.smtp_settings = {
   :address => 'smtp.gmail.com',
   :port => 587,
   :authentication => :plain,
-  :enable_starttls_auto => true
+  :enable_starttls_auto => true,
+  :openssl_verify_mode => 'none' 
 }
