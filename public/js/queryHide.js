8db01606db73c3438c023601369521f3f283a4c7
鍾賢廣 <b10215014@mail.ntust.edu.tw>
2016-12-18 19:01:55 +0800

    更改 候選議題的 隱藏 操作方式

--- public/js/queryHide.js
+++ public/js/queryHide.js
@@ -3,7 +3,13 @@ $(function(){
     $(".hideIssueButton").on("click" ,function(){
         $me = $(this)
         $Name = $me.attr("issue_id");
-        $me.hide();
+        //$me.hide();
+        
+        if ($me.attr("value") == "顯示") {
+        	$me.attr("value","隱藏")
+        }else if($me.attr("value") == "隱藏"){
+        	$me.attr("value","顯示")
+        }
         //$Name = $me.prop("value");
         $.post( "../Issuelist/hide", { Name: $Name} );
     });
