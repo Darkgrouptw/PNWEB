aec890d1dcba89f47f0f8c87c610d33b07fae044
Dark <darkgrouptw@gmail.com>
2016-12-22 20:39:21 +0800

    測試 d3js

--- public/js/Tree/EditingTree.js
+++ public/js/Tree/EditingTree.js
@@ -155,6 +155,9 @@ function makeCircleSVG(attrs, text, id, nowLevel, Degree, pos, parentID)
     tareaLink.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', "Issuelist/" + id);
     tareaLink.setAttribute("target", "_blank");
     
+    d3plus.textwrap()
+        .container(d3.select(tarea))
+        .draw();
     
     tareaLink.appendChild(tarea);
     
