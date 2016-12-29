406e882dd66ade7f4c4600886a9c47d0d3848aae
鍾賢廣 <b10215014@mail.ntust.edu.tw>
2016-12-29 15:08:30 +0800

    更新 新增意見排版

--- public/js/queryMedia.js
+++ public/js/queryMedia.js
@@ -13,6 +13,8 @@ $(function(){
                 
                 if(data.length == 1 && data[0] == "\n")
                     return;
+                if(document.getElementById("queryMediaDiv") == null)
+                    return;
                 document.getElementById("queryMediaDiv").style.display = "";
                 
                 for($i = 0; $i < data.length; $i++)
@@ -22,9 +24,36 @@ $(function(){
     });
     $("#queryMedia").on("blur", function(){
         setTimeout(function(){
+            if(document.getElementById("queryMediaDiv") == null)
+                    return;
             document.getElementById("queryMediaDiv").style.display = "none";
             $("#queryMediaDiv").empty();
         },100);
+    });
+
+    $(".mediaList").each(function(index){
+        $(this).on("focus input",function(){
+            input = this
+            name = $(this).prop("value");
+            
+            datalist = $(this).next("datalist");
+
+            $.get( "/MediaName", { name: name }).done( 
+                function( data ) {
+                    // 刪除前面的空白
+                    data = data.substr(4, data.length);
+                    // 刪除後面的換行
+                    data = data.substr(0, data.length - 1);
+                    data = data.split(",")
+                    $(datalist).empty();
+                    if(data.length == 1 && data[0] == "\n")
+                        return;
+                    for($i = 0; $i < data.length; $i++)
+                        $(datalist).append("<option value=" + data[$i] + ">");
+                    
+                }
+            );
+        });
     });
 });
 
--- public/js/queryPeople.js
+++ public/js/queryPeople.js
@@ -13,6 +13,8 @@ $(function(){
                 
                 if(data.length == 1 && data[0] == "\n")
                     return;
+                if(document.getElementById("queryPeopleDiv") == null)
+                    return;
                 document.getElementById("queryPeopleDiv").style.display = "";
                 
                 for($i = 0; $i < data.length; $i++)
@@ -22,9 +24,36 @@ $(function(){
     });
     $("#queryPeople").on("blur", function(){
         setTimeout(function(){
+            if(document.getElementById("queryPeopleDiv") == null)
+                return;
             document.getElementById("queryPeopleDiv").style.display = "none";
             $("#queryPeopleDiv").empty();
         },100);
+    });
+
+    $(".peopleList").each(function(index){
+        $(this).on("focus input",function(){
+            input = this
+            name = $(this).prop("value");
+            
+            datalist = $(this).next("datalist");
+            console.log("test");
+            $.get( "/PeopleName", { name: name }).done( 
+                function( data ) {
+                    // 刪除前面的空白
+                    data = data.substr(4, data.length);
+                    // 刪除後面的換行
+                    data = data.substr(0, data.length - 1);
+                    data = data.split(",")
+                    $(datalist).empty();
+                    if(data.length == 1 && data[0] == "\n")
+                        return;
+                    for($i = 0; $i < data.length; $i++)
+                        $(datalist).append("<option value=" + data[$i] + ">");
+                    
+                }
+            );
+        });
     });
 });
 
