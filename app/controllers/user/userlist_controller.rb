f108b2e9c0ef67dfb0d95c55260ff7ee8dd5806c
鍾賢廣 <b10215014@mail.ntust.edu.tw>
2017-04-11 21:56:10 +0800

    優化失敗

--- app/controllers/user/userlist_controller.rb
+++ app/controllers/user/userlist_controller.rb
@@ -2,47 +2,16 @@ class User::UserlistController < ApplicationController
 
 
 
-	def getScore(dataDetail,dataIssue,likeList,user)
+	def getScore(user)
 
-		details = dataDetail.where(post_id: user.id,is_report: false)
-		issue_ids = []
-		details.each do |item|
-			issue_ids.push(item.issue_id)
-		end
-		issues = dataIssue.where(id: issue_ids,is_candidate: false)
-		detailIn10Top = 0;
-
-		all_detail = details.where(issue_id: issue_ids)
-		all_detail_ids=[]
-		all_detail.each do |item|
-			all_detail_ids.push(item.id)
-		end
-		all_likes = likeList.where(detail_id: all_detail_ids)
-		details.each do |item|
-			issue = issues.where(id: item.issue_id)[0]
-			like = all_likes.where(detail_id: issue.datadetail_id.split(','))
-			details_temp = details
-			details_temp.sort_by{|item| like.where(detail_id: item.id).length}.reverse
-			if details_temp.index(details_temp.where(id: item.id)[0]) < 10
-				detailIn10Top = detailIn10Top + 1
-			end
-			#issue_list.sort_by{|item| likelist.where(detail_id: item.datadetail_id.split(',')).length}.reverse
-		end
-		postedIssueNumber = dataIssue.where(post_id: user.id,is_candidate: false).length
-
-		return detailIn10Top * 3 + details.length * 1 + @postedIssueNumber * 3
+		return getStringIDLength(user.datadetail_id)
 	end
 	def index
 		@user_order = params[:user_order]
 		@user_search = params[:user_search]
 
-		user_all = User.all
-		detail_all = DataDetail.all
-		issue_all = DataIssue.all
-		like_all = LikeList.all
-
-		@me = user_all.where(id: params[:id])[0]
-		@details = detail_all.where(post_id: @me.id,is_report: false)
+		@me = User.where(id: params[:id])[0]
+		@details = DataDetail.where(post_id: @me.id,is_report: false)
 		detail_ids = []
 		issue_ids = []
 		people_ids = []
@@ -55,56 +24,57 @@ class User::UserlistController < ApplicationController
 		@people = DataPerson.where(id: people_ids)
 		#Post.where("id = 1").or(Post.where("author_id = 3"))
 		#users.where(users[:name].eq('bob').or(users[:age].lt(25)))
-		@issues = issue_all.where(id: issue_ids,is_candidate: false)
-		@postedIssueNumber = issue_all.where(post_id: @me.id,is_candidate: false).length
-		@likedNumber = like_all.where(detail_id: detail_ids).length
-		@likeList = like_all.where(post_id: @me.id)
+		@issues = DataIssue.where(id: issue_ids,is_candidate: false)
+		@postedIssueNumber = DataIssue.where(post_id: @me.id,is_candidate: false).length
+		@likedNumber = LikeList.where(detail_id: detail_ids).length
+		@likeList = LikeList.where(post_id: @me.id)
 		@reportedNumber = ReportDetail.where(detail_id: detail_ids).length
 
-		@detailIn10Top = 0
-		@all_detail = detail_all.where(issue_id: issue_ids,is_report: false)
-		all_detail_ids=[]
-		@all_detail.each do |item|
-			all_detail_ids.push(item.id)
-		end
-		all_likes = like_all.where(detail_id: all_detail_ids)
-		if all_likes.length > 0
-			@details.each do |item|
-				issue = @issues.where(id: item.issue_id)[0]
-				if issue.nil?
-					like = all_likes.where(detail_id: -1)
-				else
-					like = all_likes.where(detail_id: issue.datadetail_id.split(','))
-				end
-				
-				details_temp = @details
-				details_temp.sort_by{|item| like.where(detail_id: item.id).length}.reverse
-				if details_temp.index(details_temp.where(id: item.id)[0]) < 10
-					@detailIn10Top = @detailIn10Top + 1
-				end
-				#issue_list.sort_by{|item| likelist.where(detail_id: item.datadetail_id.split(',')).length}.reverse
-			end
-		end
+		#@detailIn10Top = 0
+		#@all_detail = detail_all.where(issue_id: issue_ids,is_report: false)
+		#all_detail_ids=[]
+		#@all_detail.each do |item|
+		#	all_detail_ids.push(item.id)
+		#end
+		#all_likes = like_all.where(detail_id: all_detail_ids)
+		#if all_likes.length > 0
+		#	@details.each do |item|
+		#		issue = @issues.where(id: item.issue_id)[0]
+		#		if issue.nil?
+		#			like = all_likes.where(detail_id: -1)
+		#		else
+		#			like = all_likes.where(detail_id: issue.datadetail_id.split(','))
+		#		end
+		#		
+		#		details_temp = @details
+		#		details_temp.sort_by{|item| like.where(detail_id: item.id).length}.reverse
+		#		if details_temp.index(details_temp.where(id: item.id)[0]) < 10
+		#			@detailIn10Top = @detailIn10Top + 1
+		#		end
+		#		#issue_list.sort_by{|item| likelist.where(detail_id: item.datadetail_id.split(',')).length}.reverse
+		#	end
+		#end
 		
 		
-		@score = @detailIn10Top * 3 + @details.length * 1 + @postedIssueNumber * 3
+		#@score = @detailIn10Top * 3 + @details.length * 1 + @postedIssueNumber * 3
+		@score =  @details.length
 
 		if @user_search.nil? || @user_search.empty?
-			@users = user_all
+			@users = User.all
 		else
-			@users = user_all.where("nickname like ?", "%" + @user_search + "%")
+			@users = User.where("nickname like ?", "%" + @user_search + "%")
 		end
 
 		if @user_order.nil? || @user_order.empty?
 			@user_order = "hot"
 		end
 		if @user_order == "near_hot"
-			@users = @users.sort_by{|item| issue_all.where(created_at: (Time.now.in_time_zone('Taipei') - 7.day)..Time.now.in_time_zone('Taipei')).where(post_id: item.id) + detail_all.where(created_at: (Time.now.in_time_zone('Taipei') - 7.day)..Time.now.in_time_zone('Taipei')).where(post_id: item.id)}.reverse
+			@users = @users.sort_by{|item| DataIssue.where(created_at: (Time.now.in_time_zone('Taipei') - 7.day)..Time.now.in_time_zone('Taipei')).where(post_id: item.id) + DataDetail.where(created_at: (Time.now.in_time_zone('Taipei') - 7.day)..Time.now.in_time_zone('Taipei')).where(post_id: item.id)}.reverse
 			#likelist = LikeList.where( created_at: (Time.now.in_time_zone('Taipei') - 1.day)..Time.now.in_time_zone('Taipei'))
 		elsif @user_order == "hot"
-			@users = @users.sort_by{|item| issue_all.where(post_id: item.id).length + detail_all.where(post_id: item.id).length}.reverse
+			@users = @users.sort_by{|item| DataIssue.where(post_id: item.id).length + DataDetail.where(post_id: item.id).length}.reverse
 		else
-			@users = @users.sort_by{|item| getScore(detail_all.all,issue_all.all,like_all.all,item)}.reverse
+			@users = @users.sort_by{|item| getScore(item)}.reverse
 		end
 
 
